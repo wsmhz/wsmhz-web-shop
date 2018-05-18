290655a91a9c5f9ee91bb14e1c43d1a5ec576301
yddl <787740644@qq.com>
2018-05-18 17:02:35 +0800

    product:

---
+++ wsmhz-shop-back/src/main/java/com/wsmhz/web/shop/back/domain/Cart.java
@@ -0,0 +1,69 @@
+package com.wsmhz.web.shop.back.domain;
+
+import com.wsmhz.security.core.domain.common.Domain;
+
+/**
+ * create by tangbj on 2018/5/18
+ */
+public class Cart extends Domain {
+    /**
+     * Id
+     */
+    private Integer id;
+    /**
+     * 用户Id
+     */
+    private Integer userId;
+    /**
+     * 产品Id
+     */
+    private Integer productId;
+    /**
+     * 数量
+     */
+    private Integer quantity;
+    /**
+     * 勾选状态
+     */
+    private Boolean checked;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Boolean getChecked() {
+        return checked;
+    }
+
+    public void setChecked(Boolean checked) {
+        this.checked = checked;
+    }
+}
