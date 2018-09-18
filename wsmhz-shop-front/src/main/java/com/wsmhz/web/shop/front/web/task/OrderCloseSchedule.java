fd91d3190b5d56414d150ae9fdfc41e7a62772e2
wsmhz <787740644@qq.com>
2018-09-18 14:10:12 +0800

    open schedule;

--- wsmhz-shop-front/src/main/java/com/wsmhz/web/shop/front/web/task/OrderCloseSchedule.java
+++ wsmhz-shop-front/src/main/java/com/wsmhz/web/shop/front/web/task/OrderCloseSchedule.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
